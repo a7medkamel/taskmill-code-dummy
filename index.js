19b06de44c0e020cff1fd13440acc3cab5f4b8e8
Ahmed Kamel <ak@ahmedkamel.net>
2016-01-10 20:30:39 -0800

    expose sent data

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function Code(data) {
-  this.manual = data.manual;
+  this.data = data;
 };
 
 Code.prototype.run = function(req, res, next) {
-  res.send('Hi! I\'m taskmill-code-dummy, the dummy task runner');
+  res.send({
+      message : 'Hi! I\'m taskmill-code-dummy, the dummy task runner.'
+    , data    : this.data
+  });
 };
 
 module.exports = Code;
